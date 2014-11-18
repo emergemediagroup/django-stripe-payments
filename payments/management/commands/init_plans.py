8073bb0d7b664b9cb6aa969cedcbea2f2f124566
Ben Tappin <ben@mrben.co.uk>
2014-11-18 14:51:57 +0000

    Don't stop processing plans if one already exists in .

--- payments/management/commands/init_plans.py
+++ payments/management/commands/init_plans.py
@@ -13,20 +13,25 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         stripe.api_key = settings.STRIPE_SECRET_KEY
         for plan in settings.PAYMENTS_PLANS:
-            if settings.PAYMENTS_PLANS[plan].get("stripe_plan_id"):
+            stripe_plan_id = settings.PAYMENTS_PLANS[plan].get("stripe_plan_id")
+            if stripe_plan_id:
                 price = settings.PAYMENTS_PLANS[plan]["price"]
                 if isinstance(price, decimal.Decimal):
                     amount = int(100 * price)
                 else:
                     amount = int(100 * decimal.Decimal(str(price)))
 
-                stripe.Plan.create(
-                    amount=amount,
-                    interval=settings.PAYMENTS_PLANS[plan]["interval"],
-                    name=settings.PAYMENTS_PLANS[plan]["name"],
-                    currency=settings.PAYMENTS_PLANS[plan]["currency"],
-                    trial_period_days=settings.PAYMENTS_PLANS[plan].get(
-                        "trial_period_days"),
-                    id=settings.PAYMENTS_PLANS[plan].get("stripe_plan_id")
-                )
-                print("Plan created for {0}".format(plan))
+                try:
+                    stripe.Plan.retrieve(id=stripe_plan_id)
+                    print("Plan {} already exists. Skipping...".format(stripe_plan_id))
+                except stripe.InvalidRequestError:
+                    stripe.Plan.create(
+                        amount=amount,
+                        interval=settings.PAYMENTS_PLANS[plan]["interval"],
+                        name=settings.PAYMENTS_PLANS[plan]["name"],
+                        currency=settings.PAYMENTS_PLANS[plan]["currency"],
+                        trial_period_days=settings.PAYMENTS_PLANS[plan].get(
+                            "trial_period_days"),
+                        id=stripe_plan_id
+                    )
+                    print("Plan created for {0}".format(plan))
